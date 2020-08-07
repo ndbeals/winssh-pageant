f0bcdf8ee5d793d8215d20fc83f575fac8dc47ef
Nathan Beals <nathan.beals@ontariotechu.net>
2020-08-07 15:51:38 -0400

    Added flag to toggle pageant named pipes

--- main.go
+++ main.go
@@ -8,14 +8,17 @@ import (
 )
 
 var (
-	sshPipe = flag.String("sshpipe", `\\.\pipe\openssh-ssh-agent`, "Named pipe for Windows OpenSSH agent")
+	sshPipe       = flag.String("sshpipe", `\\.\pipe\openssh-ssh-agent`, "Named pipe for Windows OpenSSH agent")
+	noPageantPipe = flag.Bool("no-pageant-pipe", false, "Toggle pageant named pipe proxying")
 )
 
 func main() {
 	flag.Parse()
 
 	// Start a proxy/redirector for the pageant named pipes
-	go pipeProxy()
+	if !*noPageantPipe {
+		go pipeProxy()
+	}
 
 	pageantWindow := createPageantWindow()
 	if pageantWindow == 0 {
